78610b02698d714536611801a18670be51d4fb92
CyberneticCoding <max.pietersma@gmail.com>
2026-06-11 15:14:45 +0200

    chore: prettier configuration

--- resources/js/Core/Components/Checkbox.vue
+++ resources/js/Core/Components/Checkbox.vue
@@ -1,29 +1,29 @@
 <script setup lang="ts">
-import { computed } from 'vue';
+import { computed } from 'vue'
 
-const emit = defineEmits(['update:checked']);
+const emit = defineEmits(['update:checked'])
 
 const props = defineProps<{
-    checked: boolean;
-    value?: any;
-}>();
+    checked: boolean
+    value?: any
+}>()
 
 const proxyChecked = computed({
     get() {
-        return props.checked;
+        return props.checked
     },
 
     set(val) {
-        emit('update:checked', val);
+        emit('update:checked', val)
     },
-});
+})
 </script>
 
 <template>
-  <input
-    v-model="proxyChecked"
-    type="checkbox"
-    :value="value"
-    class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:border-gray-700 dark:bg-gray-900 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800"
-  >
+    <input
+        v-model="proxyChecked"
+        type="checkbox"
+        :value="value"
+        class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:border-gray-700 dark:bg-gray-900 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800"
+    />
 </template>
