fb3e8abb9442f6698b0b5c2bbde11102a57a4250
CyberneticCoding <max.pietersma@gmail.com>
2026-06-24 17:03:34 +0200

    feat: add BaseButton component basics

---
+++ resources/js/Core/Components/Base/BaseButton.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const props = defineProps<{
+    variant: 'primary'
+}>()
+
+const baseClasses =
+    'shadow-xs inline-flex w-full justify-center rounded-md px-3 py-2 text-sm font-semibold'
+
+const variantClasses = {
+    primary: 'bg-theme-brand-primary text-white hover:bg-theme-brand-secondary',
+}
+</script>
+
+<template>
+    <button :class="[baseClasses, variantClasses[variant]]">
+        <slot />
+    </button>
+</template>
