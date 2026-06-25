45ae71872fb67189a8f953cac386bf04fb60e11a
CyberneticCoding <max.pietersma@gmail.com>
2026-06-25 21:51:23 +0200

    refactor: miscellaneous styling tailwind adherance

--- resources/js/Core/Components/ApplicationLogo.vue
+++ resources/js/Core/Components/ApplicationLogo.vue
@@ -3,5 +3,5 @@ import logoUrl from '@/../img/maxent.svg'
 </script>
 
 <template>
-    <img :src="logoUrl" alt="Maxent Logo" class="h-6 w-auto" />
+    <img :src="logoUrl" alt="Maxent Logo" class="w-auto" />
 </template>
--- resources/js/Core/Layouts/GuestLayout.vue
+++ resources/js/Core/Layouts/GuestLayout.vue
@@ -4,17 +4,15 @@ import { Link } from '@inertiajs/vue3'
 </script>
 
 <template>
-    <div
-        class="flex min-h-screen flex-col items-center bg-gray-100 pt-12 sm:pt-24 dark:bg-gray-900"
-    >
+    <div class="flex min-h-screen flex-col items-center bg-theme-bg-tertiary pt-12 sm:pt-24">
         <div>
             <Link href="/login">
-                <ApplicationLogo class="h-20 w-20 fill-current text-gray-500" />
+                <ApplicationLogo class="size-6 fill-current" />
             </Link>
         </div>
 
         <div
-            class="mt-8 w-full overflow-hidden border-y border-theme-border-secondary bg-white p-6 shadow-lg sm:max-w-md sm:rounded-3xl sm:border dark:bg-gray-800"
+            class="mt-8 w-full overflow-hidden border-y border-theme-border-secondary bg-theme-bg-primary p-6 pb-10 shadow-lg sm:max-w-md sm:rounded-3xl sm:border"
         >
             <slot />
         </div>
