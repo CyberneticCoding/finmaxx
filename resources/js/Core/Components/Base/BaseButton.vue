cf3ed57ec423127267f9e7f842b4a650cf0821df
CyberneticCoding <max.pietersma@gmail.com>
2026-06-25 16:14:45 +0200

    feat(BaseButton): add disabled state support

--- resources/js/Core/Components/Base/BaseButton.vue
+++ resources/js/Core/Components/Base/BaseButton.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
 const props = defineProps<{
     variant: 'primary'
+    disabled?: boolean
 }>()
 
 const baseClasses =
-    'shadow-xs inline-flex w-full justify-center rounded-md px-3 py-2 text-sm font-semibold'
+    'shadow-xs inline-flex w-full justify-center rounded-md px-3 py-2 text-sm font-semibold disabled:opacity-50 disabled:hover:bg-theme-brand-primary disabled:cursor-not-allowed'
 
 const variantClasses = {
     primary: 'bg-theme-brand-primary text-white hover:bg-theme-brand-secondary',
@@ -12,7 +13,7 @@ const variantClasses = {
 </script>
 
 <template>
-    <button :class="[baseClasses, variantClasses[variant]]">
+    <button :disabled="disabled" :class="[baseClasses, variantClasses[variant]]">
         <slot />
     </button>
 </template>
