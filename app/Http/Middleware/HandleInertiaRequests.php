3a1469a160a425416de9ad9e38c0d63ab8fc0e82
CyberneticCoding <max.pietersma@gmail.com>
2026-07-01 17:40:46 +0200

    fix: handle when user is null in HandleInertiaRequests

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -34,7 +34,7 @@ class HandleInertiaRequests extends Middleware
         return [
             ...parent::share($request),
             'auth' => [
-                'user' => $request->user()->only('name', 'email'),
+                'user' => $request->user() ? $request->user()->only('name', 'email') : null,
             ],
         ];
     }
