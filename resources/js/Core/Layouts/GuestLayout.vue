cf55d9a011c138e74157cff1216d65144a4c197a
CyberneticCoding <max.pietersma@gmail.com>
2026-07-01 15:45:29 +0200

    feat(a11y): remove Logo link on GuestLayout

--- resources/js/Core/Layouts/GuestLayout.vue
+++ resources/js/Core/Layouts/GuestLayout.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import ApplicationLogo from '@/Core/Components/ApplicationLogo.vue'
-import { Link } from '@inertiajs/vue3'
 </script>
 
 <template>
@@ -8,11 +7,9 @@ import { Link } from '@inertiajs/vue3'
         <div
             class="flex h-12 w-full items-center justify-center bg-theme-brand-primary sm:mb-8 sm:mt-16 sm:h-0 sm:bg-transparent"
         >
-            <Link href="/login">
-                <ApplicationLogo
-                    class="h-4 text-white transition-all sm:h-8 sm:text-theme-text-primary"
-                />
-            </Link>
+            <ApplicationLogo
+                class="h-4 text-white transition-all sm:h-8 sm:text-theme-text-primary"
+            />
         </div>
 
         <div
