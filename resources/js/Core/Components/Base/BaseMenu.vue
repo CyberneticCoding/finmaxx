70080f3d68d921fecfe4dec0497ecba986146ce8
CyberneticCoding <max.pietersma@gmail.com>
2026-07-11 18:52:55 +0200

    feat: add support for anchor positioning in BaseMenu component

--- resources/js/Core/Components/Base/BaseMenu.vue
+++ resources/js/Core/Components/Base/BaseMenu.vue
@@ -5,6 +5,33 @@ import {
     MenuItems as HeadlessMenuItems,
 } from '@headlessui/vue'
 
+interface Props {
+    anchor?: MenuAnchor
+}
+
+type MenuAnchor =
+    | 'bottom end'
+    | 'bottom start'
+    | 'bottom'
+    | 'top end'
+    | 'top start'
+    | 'top'
+    | 'left'
+    | 'right'
+
+const { anchor = 'bottom end' } = defineProps<Props>()
+
+const anchorClasses: Record<MenuAnchor, string> = {
+    'bottom end': 'absolute right-0 top-full mt-2',
+    'bottom start': 'absolute left-0 top-full mt-2',
+    bottom: 'absolute left-1/2 -translate-x-1/2 top-full mt-2',
+    'top end': 'absolute right-0 bottom-full mb-2',
+    'top start': 'absolute left-0 bottom-full mb-2',
+    top: 'absolute left-1/2 -translate-x-1/2 bottom-full mb-2',
+    left: 'absolute right-full top-0 mr-2',
+    right: 'absolute left-full top-0 ml-2',
+}
+
 const panelClasses =
     'z-50 min-w-64 rounded-md bg-theme-bg-primary shadow-lg ring-1 ring-theme-border-secondary focus:outline-none'
 </script>
@@ -23,7 +50,7 @@ const panelClasses =
             leave-from-class="transform scale-100 opacity-100"
             leave-to-class="transform scale-95 opacity-0"
         >
-            <HeadlessMenuItems :class="panelClasses">
+            <HeadlessMenuItems :class="[panelClasses, anchorClasses[anchor]]">
                 <slot />
             </HeadlessMenuItems>
         </Transition>
